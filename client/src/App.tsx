35e49c633949d51ef45a98eeb731ac07866bc7e9
IbrahimFullStackWebDev <ibrahim.fullstack.dev@gmail.com>
2026-01-27 10:15:53 +0300

    feat: Create navbar and header component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
 import MarketPlace from "./pages/MarketPlace";
 import MyListing from "./pages/MyListing";
@@ -8,10 +8,13 @@ import ListingDetails from "./pages/ListingDetails";
 import Messages from "./pages/Messages";
 import MyOrders from "./pages/MyOrders";
 import Loading from "./pages/Loading";
+import Navbar from "./components/Navbar";
 
 const App = () => {
+  const { pathname } = useLocation();
   return (
     <div>
+      {!pathname.includes("/admin") && <Navbar />}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/marketplace" element={<MarketPlace />} />
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,9 +1,9 @@
-import React from "react";
+import Hero from "../components/Hero";
 
 const Home = () => {
   return (
     <div>
-      <h1>Home Page</h1>
+      <Hero />
     </div>
   );
 };
