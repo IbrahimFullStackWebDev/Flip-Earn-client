ad77187e6778323ff6dc1a1c3c279f772c47dc65
IbrahimFullStackWebDev <ibrahim.fullstack.dev@gmail.com>
2026-01-27 09:17:07 +0300

    feat: Create Project Structure

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,9 +3,12 @@ import { Provider } from "react-redux";
 import { store } from "./app/store.ts";
 import "./index.css";
 import App from "./App.tsx";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>,
 );
---
+++ client/src/pages/MyListing.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MyListing = () => {
+  return (
+    <div>
+      <h1>MyListing Page</h1>
+    </div>
+  );
+};
+
+export default MyListing;
